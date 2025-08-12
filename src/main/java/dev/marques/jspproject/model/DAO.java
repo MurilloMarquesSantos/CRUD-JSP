ba4a6eba746950d58e3491d105b0b9f133037b0b
Murillo Marques <murillomarques2001@gmail.com>
2025-08-12 15:23:19 -0300

    feat: add database connection

---
+++ src/main/java/dev/marques/jspproject/model/DAO.java
@@ -0,0 +1,42 @@
+package dev.marques.jspproject.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLOutput;
+
+public class DAO {
+
+    //Connection Module
+
+    private String driver = "com.mysql.cj.jdbc.Driver";
+
+    private String url = "jdbc:mysql://localhost:3306/dbagenda?useTimezone=true&ServerTimezone=UTC";
+
+    private String user = "UserJava";
+
+    private String password = System.getenv("DB_PASSWORD");
+
+
+    private Connection conectar(){
+        Connection con = null;
+        try{
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);
+            return con;
+        }catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+
+    public void testeConexao(){
+        try{
+            Connection con = conectar();
+            System.out.println(con);
+            con.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
