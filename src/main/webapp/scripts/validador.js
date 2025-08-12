d8930815960057cfe23448cc286b1a63793825d4
Murillo Marques <murillomarques2001@gmail.com>
2025-08-12 17:41:19 -0300

    feat: add email validator

--- src/main/webapp/scripts/validador.js
+++ src/main/webapp/scripts/validador.js
@@ -1,18 +1,23 @@
-function validar(){
+function validar() {
 
     let nome = frmContato.nome.value
     let fone = frmContato.fone.value
+    let email = frmContato.email.value
 
 
-    if(nome === ""){
+    if (nome === "") {
         alert("Preencha o campo nome")
         frmContato.nome.focus()
         return false;
-    }else if(fone === ""){
+    } else if (fone === "") {
         alert("Preencha o campo fone")
         frmContato.fone.focus()
         return false;
-    }else{
+    } else if (email === "") {
+        alert("Preencha o campo email")
+        frmContato.email.focus()
+        return false;
+    } else {
         document.forms["frmContato"].submit()
     }
 
