27c93942c3de3983d44c4847279f1b13f3c87924
Murillo Marques <murillomarques2001@gmail.com>
2025-08-12 17:42:26 -0300

    feat: add save contact method

--- src/main/java/dev/marques/jspproject/model/DAO.java
+++ src/main/java/dev/marques/jspproject/model/DAO.java
@@ -1,7 +1,8 @@
 package dev.marques.jspproject.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DAO {
 
@@ -16,26 +17,52 @@ public class DAO {
     private String password = System.getenv("DB_PASSWORD");
 
 
-    private Connection conectar(){
+    private Connection conectar() {
         Connection con = null;
-        try{
+        try {
             Class.forName(driver);
             con = DriverManager.getConnection(url, user, password);
             return con;
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
             return null;
         }
     }
 
-
-    public void testeConexao(){
-        try{
+    public void inserirContato(JavaBeans contatos) {
+        String create = "insert into contatos(nome,fone,email) values(?,?,?)";
+        try {
             Connection con = conectar();
-            System.out.println(con);
-            con.close();
-        }catch(Exception e){
+            PreparedStatement stmt = con.prepareStatement(create);
+            stmt.setString(1, contatos.getNome());
+            stmt.setString(2, contatos.getFone());
+            stmt.setString(3, contatos.getEmail());
+
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (Exception e) {
             System.out.println(e);
         }
+
+    }
+
+    public List<JavaBeans> getContatos() {
+        List<JavaBeans> contatos = new ArrayList<>();
+        try {
+            Connection con = conectar();
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("select * from contatos");
+            while (rs.next()) {
+                JavaBeans contato = new JavaBeans(rs.getString("idcon"),
+                        rs.getString("nome"),
+                        rs.getString("fone"),
+                        rs.getString("email"));
+                contatos.add(contato);
+                con.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return contatos;
     }
 }
