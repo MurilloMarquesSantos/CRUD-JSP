94449ae98f9822a6ca0f09bdbad8171a25af9b45
Murillo Marques <murillomarques2001@gmail.com>
2025-08-12 17:13:08 -0300

    Commit

--- src/main/java/dev/marques/jspproject/controller/Controller.java
+++ src/main/java/dev/marques/jspproject/controller/Controller.java
@@ -9,7 +9,7 @@ import dev.marques.jspproject.model.DAO;
 
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = {"/Controller", "/main", "/insert"})
 public class Controller extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
@@ -24,7 +24,17 @@ public class Controller extends HttpServlet {
         String action = request.getServletPath();
         if (action.equals("/main")) {
             contatos(request, response);
+        } else if (action.equals("/insert")) {
+            novoContato(request, response);
+        }else{
+            response.sendRedirect("index.html");
         }
+    }
+
+    protected void novoContato(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println(request.getParameter("nome"));
+        System.out.println(request.getParameter("fone"));
+        System.out.println(request.getParameter("email"));
     }
 
     protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
