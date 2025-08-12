00cf471d4b3061b52db1bcf2b067698105bdabd6
Murillo Marques <murillomarques2001@gmail.com>
2025-08-12 15:46:07 -0300

    feat: add redirect to jsp file

--- src/main/java/dev/marques/jspproject/controller/Controller.java
+++ src/main/java/dev/marques/jspproject/controller/Controller.java
@@ -5,20 +5,29 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import dev.marques.jspproject.model.DAO;
 
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/Controller", "/main"})
 public class Controller extends HttpServlet {
 
-
     private static final long serialVersionUID = 1L;
+
+    DAO dao = new DAO();
 
     public Controller() {
         super();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        String action = request.getServletPath();
+        if (action.equals("/main")) {
+            contatos(request, response);
+        }
+    }
+
+    protected void contatos(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("agenda.jsp");
     }
 }
